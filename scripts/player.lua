4512f7d95ab712d40240d7fa7b867fa000d7b7f3
Ivaylo Bachvarov <ISBachvarov21@codingburgas.bg>
2026-05-01 18:08:41 +0300

    Move movement handling to lua

--- scripts/player.lua
+++ scripts/player.lua
@@ -1,13 +1,23 @@
 function update(player, input, dt)
+    local speed = 10000
+    local jump_velocity = -1000
+
     if input.aPressed then
-        player.vel.x = player.vel.x - 600 * dt
+        player.vel.x = player.vel.x - speed * dt
     end
 
     if input.dPressed then
-        player.vel.x = player.vel.x + 600 * dt
+        player.vel.x = player.vel.x + speed * dt
     end
 
-    if input.jump and player.onGround then
-        player.vel.y = -980
+    if input.jump then
+        print("jumping")
+        player:apply_velocity(Vec2:new(0, jump_velocity))
+    end
+
+    if player.pos.x > 1280 then
+        player:set_position(Vec2:new(-50, player.pos.y))
+    elseif player.pos.x < -50 then
+        player:set_position(Vec2:new(1280-50, player.pos.y))
     end
 end
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,33 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
 #include <algorithm>
+#include <filesystem>
+
+namespace fs = std::filesystem;
+
+fs::file_time_type last_write_time;
+sol::function update_fn;
+sol::state lua;
+
+void load_script() {
+	lua.safe_script_file("../../scripts/player.lua");
+	update_fn = lua["update"];
+}
+
+void try_reload() {
+	auto current = fs::last_write_time("../../scripts/player.lua");
+
+	if (current != last_write_time) {
+		last_write_time = current;
+
+		try {
+			load_script();
+			std::cout << "Lua reloaded\n";
+		} catch (const std::exception& e) {
+			std::cout << "Lua error: " << e.what() << "\n";
+		}
+	}
+}
 
 struct Input {
 	bool aPressed = false;
@@ -15,11 +42,10 @@ struct Player {
 	bool onGround = false;
 	sf::RectangleShape shape;
 
-	void apply_velocity(const sf::Vector2f v) {
-		vel.x += v.x;
-		vel.y += v.y;
+	void applyVelocity(const sf::Vector2f v) {
+		vel += v;
 	}
-	void set_velocity(const sf::Vector2f v) {
+	void setVelocity(const sf::Vector2f v) {
 		vel = v;
 	}
 	void move(const sf::Vector2f v) {
@@ -28,15 +54,26 @@ struct Player {
 	}
 	void setPosition(const sf::Vector2f v) {
 		pos = v;
-		shape.setPosition(v);
+		shape.setPosition(pos);
 	}
 };
 
 int main() {
-	// sol::state lua;
-	// lua.open_libraries(sol::lib::base);
-	//
-	// lua.script("print('Lua works')");
+	std::cout << "Opening libraries" << std::endl;
+	lua.open_libraries(sol::lib::base, sol::lib::math);
+
+	std::cout << "Exposing usertypes" << std::endl;
+	lua.new_usertype<sf::Vector2f>("Vec2",
+		sol::constructors<sf::Vector2f(), sf::Vector2f(float, float)>(),
+		"x", &sf::Vector2f::x,
+		"y", &sf::Vector2f::y
+	);
+	lua.new_usertype<Input>("Input",
+		"aPressed", &Input::aPressed,
+		"dPressed", &Input::dPressed,
+		"jump", &Input::jump
+	);
+
 
 	sf::RenderWindow window(sf::VideoMode({1280, 720}), "SFML works");
 
@@ -47,13 +84,31 @@ int main() {
 	player.shape.setPosition({100, 100});
 	player.shape.setFillColor(sf::Color::Red);
 
+	lua.new_usertype<Player>("Player",
+		"pos", &Player::pos,
+		"vel", &Player::vel,
+		"onGround", &Player::onGround,
+		"apply_velocity", &Player::applyVelocity,
+		"set_velocity", &Player::setVelocity,
+		"move", &Player::move,
+		"set_position", &Player::setPosition
+	);
+
 	Input input;
 
 	sf::Clock clock;
 	float dt = clock.restart().asSeconds();
-	bool isJumping = false;
+	float timeSinceLastReload = 0;
 
+	std::cout << "Entering game loop" << std::endl;
+
+	window.setFramerateLimit(60);
 	while (running) {
+		if (timeSinceLastReload > 1) {
+			try_reload();
+			timeSinceLastReload = 0;
+		}
+
 		while (const std::optional event = window.pollEvent()) {
 			if (event->is<sf::Event::Closed>()) {
 				running = false;
@@ -61,22 +116,24 @@ int main() {
 			}
 		}
 
-		input = {false, false, input.jump};
+		input = {false, false, false};
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::A)) {
 			input.aPressed = true;
-			player.vel.x -= 60;
 		}
 		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::D)) {
 			input.dPressed = true;
-			player.vel.x += 60;
 		}
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W) && !input.jump) {
+		if (sf::Keyboard::isKeyPressed(sf::Keyboard::Key::W) && player.onGround) {
 			input.jump = true;
-			player.vel.y = -980;
+			player.onGround = false;
+		}
+
+		if (update_fn.valid()) {
+			update_fn(&player, input, dt);
 		}
 
 		// physics
-		player.vel.y += 3920 * dt;
+		player.applyVelocity({0, 3920 * dt});
 		if (!input.aPressed && !input.dPressed) {
 			player.vel.x *= 0.1;
 		}
@@ -90,7 +147,7 @@ int main() {
 		if (player.pos.y > 620.f) {
 			player.setPosition({player.pos.x, 620.f});
 			player.vel.y = 0;
-			input.jump = false;
+			player.onGround = true;
 		}
 
 		window.clear();
@@ -98,6 +155,7 @@ int main() {
 		window.display();
 
 		dt = clock.restart().asSeconds();
+		timeSinceLastReload += dt;
 	}
 
 	return 0;
